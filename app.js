36e86a7d88824ea7efd3bbea6c8aaa782a48da5c
Giorgio Gristina <63541341+GiorgioGristina@users.noreply.github.com>
2020-09-02 11:38:54 +0200

    started the get random function

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ let phrase = document.getElementById("phrase");
 const buttonReset = document.querySelector(".btn__reset");
 const overlay = document.getElementById("overlay");
 let missedGuess = 0;
+let ul = phrase.querySelector("ul");
 //created array w phrases to guess;
 let phrases = ["blackcurrent", "book shop", "the mouline rouge", "red wine", "rolling paper"];
 
-//add event listner to start the Game
+//add event listner to start the Game, so this will remove the-
+//overlay when the reset button has been clicked
 buttonReset.addEventListener("click", () => {
   overlay.style.display = "none";
 });
+
+// function for random phrases
+function stub (arr) {
+  // random number between 0 and the length of the array
+  let randomNumb = math.floor(math.random * phrases.length)
+  // get the random index number
+  phrases[randomNumb.value];
+};
