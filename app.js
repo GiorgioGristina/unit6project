d13ba6f2092b6e6b3a8bf069e8a3b97191a64677
Giorgio Gristina <63541341+GiorgioGristina@users.noreply.github.com>
2020-09-02 10:52:50 +0200

    created the phrases array

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+// declaring variables
 let qwerty = document.getElementById("qwerty");
 let phrase = document.getElementById("phrase");
+const buttonReset = document.querySelector(".btn__reset");
+let missedGuess = 0;
+
+let phrases = ["blackcurrent", "book shop", "the mouline rouge", "red wine", "rolling paper"];
