72803d4538336eb14b9bdfcbd2beb648c2d12756
Giorgio Gristina <63541341+GiorgioGristina@users.noreply.github.com>
2020-09-02 11:08:57 +0200

    add event listner btn reset to remove overlay

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 let qwerty = document.getElementById("qwerty");
 let phrase = document.getElementById("phrase");
 const buttonReset = document.querySelector(".btn__reset");
+const overlay = document.getElementById("overlay");
 let missedGuess = 0;
-
+//created array w phrases to guess;
 let phrases = ["blackcurrent", "book shop", "the mouline rouge", "red wine", "rolling paper"];
+
+//add event listner to start the Game
+buttonReset.addEventListener("click", () => {
+  overlay.style.display = "none";
+});
